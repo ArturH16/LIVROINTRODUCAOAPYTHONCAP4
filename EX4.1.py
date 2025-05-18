fa4d6f89a331a168156a9f6f97664375e53909d0
ArturH16 <holandaartur163@gmail.com>
2025-05-18 11:52:46 -0300

    Create EX4.1.py

---
+++ EX4.1.py
@@ -0,0 +1,8 @@
+#ANALISE O PROGRAMA 4.1. RESPONDA O QUE ACONTECE SE O PRIMEIRO E O SEGUNDO VALORES FOREM IGUAIS? EXPLIQUE
+a = int(input('Digite o primeiro valor: '))
+b = int(input('Digite o segundo valor: '))
+if a > b:
+    print('O primeiro valor é MAIOR')
+if a < b:
+    print('O segundo valor é MAIOR')
+#NADA SERÁ EXECUTADO, JÁ QUE AS DUAS CONDIÇÕES RESULTARAM EM FALSO
